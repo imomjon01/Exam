dc3421243fa86481562d947b38fb83da9630cf5f
macbook.u <imomjonuzb@gmail.com>
2025-04-29 14:31:30 +0500

    pull qilib ishni boshlalaring

--- src/main/java/uz/pdp/project/config/SecurityConfig.java
+++ src/main/java/uz/pdp/project/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package uz.pdp.project.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,5 +32,9 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
