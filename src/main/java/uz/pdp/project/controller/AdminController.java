be7235fbdc69ddb822ac48c94e0d3acf364e281b
macbook.u <imomjonuzb@gmail.com>
2025-05-01 20:01:33 +0500

    user crud update

--- src/main/java/uz/pdp/project/controller/AdminController.java
+++ src/main/java/uz/pdp/project/controller/AdminController.java
@@ -5,6 +5,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uz.pdp.project.entity.User;
 import uz.pdp.project.repo.UserRepository;
@@ -20,11 +22,27 @@ public class AdminController {
 
     @GetMapping
     public String admin(Model model) {
-        List<User> all = userRepository.findAll();
+        List<User> allUsers = userRepository.findAll();
+        String currentUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        // Login qigan user berilmaydi sababi u ozini rolini adminlikdan olib tashlasa admin pagega hich kim kira olmaydi
+        List<User> filteredUsers = allUsers.stream()
+                .filter(user -> !user.getUsername().equals(currentUsername))
+                .toList();
 
-      //  Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("users", all);
+        model.addAttribute("currentUsername", currentUsername);
+        model.addAttribute("users", filteredUsers);
         return "admin";
     }
+
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id, Model model) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.delete(user.get());
+        }
+        return "redirect:/admin";
+    }
+
 
 }
