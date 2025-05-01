0fe54b01d0f038ffe3b52114c154bf8d3c41254a
mirza <mirzabeknormakhmatov03@gmail.com>
2025-05-01 19:59:43 +0500

    Role change ozgartirildi

--- src/main/java/uz/pdp/project/controller/AdminController.java
+++ src/main/java/uz/pdp/project/controller/AdminController.java
@@ -4,9 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.project.entity.Role;
 import uz.pdp.project.entity.User;
+import uz.pdp.project.repo.RoleRepository;
 import uz.pdp.project.repo.UserRepository;
 
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.Optional;
 @RequestMapping("/admin")
 public class AdminController {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @GetMapping
     public String admin(Model model) {
@@ -26,5 +28,28 @@ public class AdminController {
         model.addAttribute("users", all);
         return "admin";
     }
+
+    @GetMapping("/change-role/{id}")
+    public String showRoleForm(@PathVariable Integer id, Model model) {
+        User user = userRepository.findById(id).orElseThrow();
+        List<Role> allRoles = roleRepository.findAll();
+        model.addAttribute("user", user);
+        model.addAttribute("roles", allRoles);
+        return "change-role";
+    }
+
+    @PostMapping("/change-role/{id}")
+    public String updateUserRoles(@PathVariable Integer id,
+                                  @RequestParam("roleIds") List<Integer> roleIds) {
+        User user = userRepository.findById(id).orElseThrow();
+        List<Role> selectedRoles = roleRepository.findAllById(roleIds);
+        user.setRoles(selectedRoles); // eski rollarni almashtiramiz
+        userRepository.save(user);
+        return "redirect:/admin";
+    }
+
+
+
+
 
 }
